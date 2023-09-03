755c9b68a7686fa491006c4db55c739275601008
Tarun <143323486+Tarun0008@users.noreply.github.com>
2023-09-03 11:42:11 +0530

    Time: 7 ms (26.91%) | Memory: 9.7 MB (15.81%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -3,16 +3,11 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         int val=0;
         for(int i=0;i<nums.size();i++)
-        {
             if(nums[i]==target)
             val=i;
-            else{
+            else
                 if(nums[i]<target)
-                {
                     val=i+1;
-                }
-            }
-        }
 return val;
     }
 };
