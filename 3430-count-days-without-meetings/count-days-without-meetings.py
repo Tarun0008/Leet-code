9847fae300c9b4e0f74b95729b530e92f6f7d546
Tarun <143323486+Tarun0008@users.noreply.github.com>
2025-03-24 20:00:39 +0530

    Time: 327 ms (7.55%) | Memory: 52.6 MB (90.93%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.py
+++ 3430-count-days-without-meetings/count-days-without-meetings.py
@@ -16,3 +16,13 @@ class Solution:
         return wd
 
 
+        # for start,end in (meetings):
+        #     if not nm or nm[-1][1]<start:
+        #         nm.append([start,end])
+        #     else:
+        #         nm[-1][1]=max(nm[-1][1],end)
+        # print(nm)
+        # for start,end in nm:
+        #     wd+=end-start+1
+        # return days-wd
+
