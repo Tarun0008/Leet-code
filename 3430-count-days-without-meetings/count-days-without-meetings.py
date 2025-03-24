76dc3cb3e7d2d6ab3eea6b56f8dc6825318f6b66
Tarun <143323486+Tarun0008@users.noreply.github.com>
2025-03-24 20:02:56 +0530

    Time: 167 ms (66.75%) | Memory: 52.8 MB (55.42%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.py
+++ 3430-count-days-without-meetings/count-days-without-meetings.py
@@ -2,7 +2,6 @@ class Solution:
     def countDays(self, days: int, meetings: List[List[int]]) -> int:
 
         meetings.sort()
-        print(meetings)
         nm=[]
         wd=0
         m=0
@@ -15,14 +14,4 @@ class Solution:
         wd+=days-m
         return wd
 
-
-        # for start,end in (meetings):
-        #     if not nm or nm[-1][1]<start:
-        #         nm.append([start,end])
-        #     else:
-        #         nm[-1][1]=max(nm[-1][1],end)
-        # print(nm)
-        # for start,end in nm:
-        #     wd+=end-start+1
-        # return days-wd
 
