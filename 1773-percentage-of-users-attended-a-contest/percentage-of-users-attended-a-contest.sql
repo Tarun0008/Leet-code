da32dd3e91fa29caaa4a576b35ba725748845787
Tarun <143323486+Tarun0008@users.noreply.github.com>
2025-04-08 20:35:16 +0530

    Time: 1203 ms (73.38%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,5 +1,5 @@
 select b.contest_id, round(count(distinct b.user_id)*100/(select count(*) from users),2) as percentage from 
 Register b
 group by b.contest_id
- order by percentage desc,
- b.contest_id asc;
+order by percentage desc,
+b.contest_id asc;
