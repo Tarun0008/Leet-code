d59a740c90d4419cf8f4d19b7d4aa9529ce6ed47
Tarun <143323486+Tarun0008@users.noreply.github.com>
2025-06-17 21:50:29 +0530

    Time: 10 ms (88.43%) | Memory: 20.1 MB (55.65%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.py
+++ 485-max-consecutive-ones/max-consecutive-ones.py
@@ -5,11 +5,7 @@ class Solution:
         for i in range(len(nums)):
             if nums[i]==1:
                 temp=temp+1
-                print("temp",temp)
             else:
-                print("max",maxx)
-                print("temp2",temp)
                 maxx=max(maxx,temp)
-
                 temp=0
         return max(maxx,temp)
