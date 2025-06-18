6c0f1311b009d6e5e3887e36dbcad94038f6a918
Tarun <143323486+Tarun0008@users.noreply.github.com>
2025-06-18 11:15:22 +0530

    Time: 1442 ms (88.31%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -0,0 +1,8 @@
+select * from
+
+(select 'Low Salary'as category ,count(account_id) as accounts_count from Accounts where income<20000
+union
+select 'Average Salary' as category , count(account_id) as accounts_count from Accounts where income between 20000 and 50000
+union
+select 'High Salary'as category ,count(account_id) as accounts_count from Accounts where income>50000) A
+order by A.accounts_count desc;
