a5698af6555fc0670730dd9d8cdf8b421a21142a
Tarun <143323486+Tarun0008@users.noreply.github.com>
2025-07-29 22:26:53 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (30.89%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -1,3 +1,9 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        return set('abcdefghijklmnopqrstuvwxyz').issubset(set(sentence.lower()))
+        s=set(sentence)
+        if len(s)==26:
+            return True
+        else:
+            return False
+        
+        # return set('abcdefghijklmnopqrstuvwxyz').issubset(set(sentence.lower()))
