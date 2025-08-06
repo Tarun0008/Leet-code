15121eb1f1b9b431a8d467bea75493fc83df9bae
Tarun <143323486+Tarun0008@users.noreply.github.com>
2025-08-06 22:37:29 +0530

    Time: 0 ms (100.00%) | Memory: 25.4 MB (92.38%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,17 +1,21 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        if len(nums)==1:
-            return nums
-        k=k%len(nums)
-        ans=[]
-        ans2=[]
-        q=deque()
-        for i in range(len(nums)-k):
-           ans.append(nums[i])
-        for i in range(len(nums)-k,len(nums)):
-            ans2.append(nums[i])
-        q.extend(ans)
-        for i in reversed(ans2):
-            q.appendleft(i)
-        nums[:]=list(q)
-        print(list(q))
+        n=len(nums)
+        k=k%n
+        nums[:]=nums[-k:]+nums[:-k]
+
+        # if len(nums)==1:
+        #     return nums
+        # k=k%len(nums)
+        # ans=[]
+        # ans2=[]
+        # q=deque()
+        # for i in range(len(nums)-k):
+        #    ans.append(nums[i])
+        # for i in range(len(nums)-k,len(nums)):
+        #     ans2.append(nums[i])
+        # q.extend(ans)
+        # for i in reversed(ans2):
+        #     q.appendleft(i)
+        # nums[:]=list(q)
+
