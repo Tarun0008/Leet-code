2d56fe72d084e388353108cc7a18b5612016428b
Tarun <143323486+Tarun0008@users.noreply.github.com>
2025-08-06 21:47:10 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (18.34%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        d={}
+
+        for num in nums:
+            if num not in d:
+                d[num]=1
+            else:
+                if d[num]==1:
+                    d[num]+=1
+                else:
+                    del d[num]
+        return list(d.keys())[0]
+                
