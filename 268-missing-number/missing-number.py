40a3e59b65bd8829ac6e76b62a8434024bc4ca10
Tarun <143323486+Tarun0008@users.noreply.github.com>
2025-08-27 17:51:53 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (33.16%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,14 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-
         n=len(nums)
-    
-        a=n*(n+1)//2
-        print(a)
-        
-        b=sum(nums)
-        print(b)
-
-        return a-b
-        
-        
+        s=sum(nums)
+        t=n*(n+1)//2
+        return t-s
